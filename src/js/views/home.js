89a58f4fb78c08503685fc00687868ab7453914a
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-07-18 20:17:47 +0000

    lunes

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,13 +4,149 @@ import "../../styles/home.css";
 import { Link } from "react-router-dom";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<Link to="/" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</Link>
+	<>
+    <div class="container-fluid bg-dark text-light p-5">
+    <div class="container bg-dark p-5">
+        <h1 class="display-4">Welcome to my Website</h1>
+        <br></br>
+        <p>Here is the Starwars Characters, Planets and Starships</p>
+        <Link to="#" class="btn btn-info">May the force be with you</Link>
+    </div>
 	</div>
+	<div className="Title">
+		<h1>Characters</h1>
+	</div>
+    <div className="card-group">
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This card has supporting text below as a natural lead-in to
+            additional content.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </div>
+	<div className="Title">
+		<h1>Planets</h1>
+	</div>
+	<div className="card-group">
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This card has supporting text below as a natural lead-in to
+            additional content.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </div>
+	<div className="Title">
+		<h1>Starships</h1>
+	</div>
+	<div className="card-group">
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This card has supporting text below as a natural lead-in to
+            additional content.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </>
 );
--- src/js/views/people.js
+++ src/js/views/people.js
@@ -2,7 +2,63 @@ import React from 'react'
 
 const People=() => {
   return (
-    <div>People</div>
+    <>
+      <div>People</div>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Link to="/" class="btn btn-primary">Go Home</Link>
+    </>
   )
 }
 export default People;
