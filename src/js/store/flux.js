d7a3f6989364d91d620bc0b663869e00b0fe2f03
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-07-19 22:43:36 +0000

    martes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,48 +1,53 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
+  return {
+    store: {
+
+      demo: [
+        {
+            apiURL: "https://swapi.dev/api/people/",
+            title: "FIRST",
+            background: "white",
+            initial: "white"
+        },
+        {
+            title: "SECOND",
+            background: "white",
+            initial: "white"
+        }
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+      loadSomeData: () => {
+        /**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
 
-				//reset the global store
-				setStore({ demo: demo });
-			},
-			getPeople: () => {
-				
-			}
-		}
-	};
+        //reset the global store
+        setStore({ demo: demo });
+      },
+      getPeople: () => {
+        const {apiURL} = getStore()
+        fetch(`${apiURL}/users`)
+        .then((response) => response.json())
+        .then(data => setStore({ users: data }))
+      },
+    },
+  };
 };
 
 export default getState;
