93b0091646d0dc7ef5bc4394748c6c8de0255c77
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-07-19 03:20:39 +0000

    lunes

--- src/js/views/NotFound.js
+++ src/js/views/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-      <div className="container">
+      <div className="container text-align-center">
           <div className="row">
               <div className="col-md-12">
                   <h1>Page Not Found (404)</h1>
